d127e2ddeba7cddd7ea7cc543903ea257b4fa87c
Paweł Gawędzki <precpaw@op.pl>
2016-12-02 21:07:55 +0100

    PropertiesHelper created.

---
+++ src/pg/helper/PropertiesHelper.java
@@ -0,0 +1,67 @@
+package pg.helper;
+
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import pg.constant.ProgramConstants;
+import pg.exception.ErrorCode;
+import pg.exception.ProgramException;
+
+/**
+ * @author Gawa [Paweł Gawędzki]
+ */
+public class PropertiesHelper {
+
+    private boolean anyPropertyChanged;
+
+    public Properties loadProgramProperties(String filePath) throws ProgramException {
+        if (filePath == null || filePath.length() == 0) {
+            return null;
+        }
+        Properties props = new Properties();
+        try (Reader reader = new FileReader(filePath)){
+            props.load(reader);
+        } catch (Exception ex) {
+            throw new ProgramException(ErrorCode.LOAD_PROPERTIES, ex);
+        }
+        return props;
+    }
+
+    public void saveProgramProperties(Properties props, String filePath) throws ProgramException {
+        try (Writer writer = new PrintWriter(filePath)){
+            props.store(writer, "");
+            writer.flush();
+        } catch (Exception ex) {
+            throw new ProgramException(ErrorCode.SAVE_PROPERTIES, ex);
+        }
+    }
+
+    public void changeProperty(Properties properties, String key, String valueToBeAdded) {
+        String newValue = String.format("%s%s", properties.getProperty(key), valueToBeAdded);
+        properties.setProperty(key, newValue);
+        anyPropertyChanged = true;
+    }
+
+    public void overwriteProperty(Properties properties, String key, String newValue) {
+        properties.setProperty(key, newValue);
+        anyPropertyChanged = true;
+    }
+
+    public boolean isAnyPropertyChanged() {
+        return anyPropertyChanged;
+    }
+    
+    public static ResourceBundle readBundles() {
+        ResourceBundle bundle = ResourceBundle.getBundle(ProgramConstants.BUNDLE_PATH);
+        if (bundle == null) {
+            System.err.printf("Error during bundle loading. Program will exit.%n");
+            System.exit(-1);
+        }
+        MessageHelper helper = new MessageHelper(bundle);
+        System.out.println(helper.getFullMessage("bundle.loaded"));
+        return bundle;
+    }
+}
---
+++ test/pg/helper/PropertiesHelperTest.java
@@ -0,0 +1,68 @@
+package pg.helper;
+
+import java.net.URL;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pg.constant.ProgramConstants;
+
+/**
+ * @author Gawa [Paweł Gawędzki]
+ * @date 2016-11-23 20:29:01
+ */
+public class PropertiesHelperTest {
+
+    private PropertiesHelper helper;
+    private ClassLoader classLoader;
+
+    @Before
+    public void setUp() throws Exception {
+        helper = new PropertiesHelper();
+        classLoader = this.getClass().getClassLoader();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        helper = null;
+        classLoader = null;
+    }
+
+    @Test
+    public void givenPropertyFileWhenLoadProgramPropertiesThenFileLoaded() throws Exception {
+        URL url = classLoader.getResource(ProgramConstants.APP_CONFIG_PATH);
+        Properties properties = helper.loadProgramProperties(url.getPath());
+        assertEquals("Props should contains value.", properties.getProperty("core.name.used"), "wakacje");
+        String fileConnectors = properties.getProperty("file.connectors");
+        assertEquals("Props should contains value.", fileConnectors, "- +,_.");
+    }
+    
+    @Test
+    public void givenBoundleFileWhenReadBoundleThenSuccess() {
+        ResourceBundle bundle = PropertiesHelper.readBundles();
+        assertNotNull("Bundle should be created.", bundle);
+    }
+
+    @Test
+    public void testLoadProgramProperties() throws Exception {
+    }
+
+    @Test
+    public void testSaveProgramProperties() throws Exception {
+    }
+
+    @Test
+    public void testChangeProperty() {
+    }
+
+    @Test
+    public void testOverwriteProperty() {
+    }
+
+    @Test
+    public void testIsAnyPropertyChanged() {
+    }
+
+}
