adb53a60cd1f611a6bcbd313015d05e242708623
Pawe?? GawÄ?dzki <precpaw@op.pl>
2016-12-02 21:10:53 +0100

    Adjusting the view.

---
+++ src/pg/view/controller/AbstractController.java
@@ -0,0 +1,31 @@
+package pg.view.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import pg.helper.ResourceHelper;
+import pg.logger.impl.ConsoleLogger;
+import pg.helper.MessageHelper;
+import pg.logger.AppLogger;
+
+/**
+ * @author Gawa [Paweł Gawędzki]
+ * 2016-12-02 22:05:20
+ */
+public abstract class AbstractController implements Initializable {
+    protected ResourceBundle bundle;
+    protected URL location;
+    protected ResourceHelper controllerHelper;
+    protected MessageHelper messageHelper;
+    protected AppLogger logger;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.location = location;
+        bundle = resources;
+        controllerHelper = new ResourceHelper();
+        messageHelper = MessageHelper.getInstance(bundle);
+        logger = new ConsoleLogger(messageHelper);
+    }
+
+}
--- src/pg/view/controller/StartController.java
+++ src/pg/view/controller/StartController.java
@@ -6,19 +6,22 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import static pg.constant.ProgramConstants.IMG_PATH;
+import pg.exception.ProgramException;
 
 /**
  * @author Gawa [Paweł Gawędzki]
  * 2016-11-21 20:34:16
  */
-public class StartController implements Initializable {
+public class StartController extends AbstractController {
     
     @FXML private TextField coreNameTextField;
     @FXML private ComboBox<String> fileConnectorComboBox;
@@ -36,9 +39,27 @@ public class StartController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
         List<String> list = Arrays.asList(new String[]{"", "-", "_", "+"});
         coreNameTextField.setText("Test");
         fileConnectorComboBox.setItems(FXCollections.observableList(list));
+        setUpButtons();
+    }
+
+    private void setUpButtons() {
+        Image buttonImage;
+        try {
+            buttonImage = controllerHelper.readImage(IMG_PATH + "source.png");
+            sourceButton.setGraphic(new ImageView(buttonImage));
+        } catch (ProgramException ex) {
+            logger.log(ex);
+        }
+        try {
+            buttonImage = controllerHelper.readImage(IMG_PATH + "destination.png");
+            destinationButton.setGraphic(new ImageView(buttonImage));
+        } catch (ProgramException ex) {
+            logger.log(ex);
+        }
     }
 
 }
